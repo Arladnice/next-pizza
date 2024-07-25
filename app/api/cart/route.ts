0e1422b7b90c6ff02163a1896268fee47d5b8e20
Denis Arlakov <denarlakov12@gmail.com>
2024-07-25 14:09:23 +0300

    POST cart 1:39:20

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,6 +1,9 @@
 import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 import crypto from 'crypto';
+import { findOrCreateCart } from '@/shared/lib/find-or-create-cart';
+import { CreateCartItemValues } from '@/shared/services/dto/cart.dto';
+import { updateCartTotalAmount } from '@/shared/lib/update-cart-total-amount';
 
 export async function GET(req: NextRequest) {
   try {
@@ -50,8 +53,51 @@ export async function POST(req: NextRequest) {
       token = crypto.randomUUID();
     }
 
-    
+    const userCart = await findOrCreateCart(token);
 
+    const data = (await req.json()) as CreateCartItemValues;
+
+    const findCartItem = await prisma.cartItem.findFirst({
+      where: {
+        cartId: userCart.id,
+        productItemId: data.productItemId,
+        ingredients: {
+          every: {
+            id: {
+              in: data.ingredients,
+            },
+          },
+        },
+      },
+    });
+
+    // Если товар был найден, делаем +1
+    if (findCartItem) {
+      const updatedCartItem = await prisma.cartItem.update({
+        where: {
+          id: findCartItem.id,
+        },
+        data: {
+          quantity: findCartItem.quantity + 1,
+        },
+      });
+    }
+
+    await prisma.cartItem.create({
+      data: {
+        cartId: userCart.id,
+        productItemId: data.productItemId,
+        quantity: 1,
+        ingredients: {
+          connect: data.ingredients?.map((id) => ({ id })),
+        },
+      },
+    });
+
+    const updatedUserCart = await updateCartTotalAmount(token);
+    const resp = NextResponse.json(updatedUserCart);
+    resp.cookies.set('cartToken', token);
+    return resp;
   } catch (error) {
     console.log('[CART_POST] Server error: ', error);
     return NextResponse.json({ message: 'Не удалось создать корзину' }, { status: 500 });
